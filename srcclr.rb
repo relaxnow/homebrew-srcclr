6254684bf61b7b4c35cbddfcfcf6bcfd4c0913f7
Boy Baukema <boy@relaxnow.nl>
2023-02-23 16:53:58 +0100

    Update srcclr.rb

--- srcclr.rb
+++ srcclr.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Srcclr < Formula
-  VERSION = "3.8.20"
+  VERSION = "3.8.18"
 
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
   url "https://download.srcclr.com/srcclr-#{VERSION}-macosx.tgz"
-  sha256 "ab71eb9e78fdf659499d26f34f884f3c8ca57ca1eb5e206046d891829ce98470"
+  sha256 "64c7219c961a8f142ad14e2ee6cdbd7698983a27d45e89642a8bb4df2abbb53a"
 
   def install
     prefix.install 'THIRD-PARTY.txt'
